98723565b9339828c3aec4a669d872cb0defeb36
Bednaz98 <30347432+Bednaz98@users.noreply.github.com>
2022-01-27 15:32:01 -0500

    modified:   Classes/RequestBuilder.ts

--- Classes/RequestBuilder.ts
+++ Classes/RequestBuilder.ts
@@ -5,7 +5,7 @@ import { v4 } from "uuid";
 export default class RequestBuilder{
     private BuildRequest:Request;
     constructor(ManagerID:string, EmployeeID:string, Amount:number){
-        const TempRequestMan:RequestManager = new RequestManager({  Amount, RequestStatus:0, PostDate:  (new Date).getSeconds()})
+        const TempRequestMan:RequestManager = new RequestManager({  Amount, RequestStatus:0, PostDate:  Date.now()})
         this.BuildRequest = TempRequestMan.DeconstructObject();
         this.BuildRequest.id = `${ManagerID}~${EmployeeID}~${v4()}`
     }
